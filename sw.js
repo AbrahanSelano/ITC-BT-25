97875553b2a9180592134e782caaee3e0769768b
AbrahanSelano <karhim2112@gmail.com>
2026-06-28 10:17:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const CACHE = 'itcbt25-v1';
-const ASSETS = ['./index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'itcbt25-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './esquema-basico.png',
+  './esquema-elevado.png'
+];
+self.addEventListener('install', e => e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(ASSETS))
+));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  ))
+));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+));
