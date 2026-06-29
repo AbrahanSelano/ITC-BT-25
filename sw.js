6ade03782eb1443921e95e88eb1d6bb50cf7ec63
AbrahanSelano <karhim2112@gmail.com>
2026-06-29 18:05:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE = 'itcbt25-v2';
+const CACHE = 'itcbt25-v3';
 const ASSETS = [
   './index.html',
   './manifest.json',
   './esquema-basico.png',
-  './esquema-elevado.png'
+  './esquema-elevado.png',
+  './instalaciones-sanitarias.pdf'
 ];
 self.addEventListener('install', e => e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(ASSETS))
